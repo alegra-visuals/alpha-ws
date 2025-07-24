ee88bca451e62fba732f60ec85118c21ec86c5ac
ALEGRA <alegrabangaluru@gmail.com>
2025-07-24 16:43:50 +0530

    add api

--- src/webp/Controller.jsx
+++ src/webp/Controller.jsx
@@ -5,7 +5,7 @@ function Controller() {
      const [socket,setSocket] = useState(null);
     useEffect(()=>{
         // const newsocket = io("http://127.0.0.1:8080/")
-         const newsocket = io("https://api.alegravisuals.com:8080/")
+         const newsocket = io("https://api.alegravisuals.com/")
         newsocket.on("connect",()=>{
             console.log("connected");
             setSocket(newsocket);
--- src/webp/CubeExperiance.jsx
+++ src/webp/CubeExperiance.jsx
@@ -6,7 +6,7 @@ function CubeExperiance() {
     const [socket,setSocket] = useState(null);
        useEffect(()=>{
         //    const newsocket = io("http://127.0.0.1:8080/")
-           const newsocket = io("https://api.alegravisuals.com:8080/")
+           const newsocket = io("https://api.alegravisuals.com/")
            newsocket.on("connect",()=>{
                console.log("connected");
             //    alert("right");
